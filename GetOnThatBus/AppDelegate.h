cecf42f374ccfc1e0131ec61e8e3f4401af362ce
Leandro Pessini <leandro@pessini.me>
2015-03-24 14:48:46 -0300

    Initial Commit

---
+++ GetOnThatBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetOnThatBus
+//
+//  Created by Leandro Pessini on 3/24/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
