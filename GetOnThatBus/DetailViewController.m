04ca9f65aaf40110e9533766f27b2d27e997429e
Leandro Pessini <leandro@pessini.me>
2015-03-28 16:10:56 -0300

    Add a detail view controller to show the bus stops details

--- GetOnThatBus/DetailViewController.m
+++ GetOnThatBus/DetailViewController.m
@@ -10,14 +10,62 @@
 
 @interface DetailViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *busStopImageView;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *routesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *directionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *intermodalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleIntermodalLabel;
+
 @end
 
 @implementation DetailViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    [self loadInfos];
+}
 
-    
+#pragma mark - Helper Methods
+
+- (void)loadInfos
+{
+    self.routesLabel.text = self.busStop.routes;
+    self.directionLabel.text = self.busStop.direction;
+    self.intermodalLabel.text = self.busStop.intermodal;
+    [self setAddressFromLatidudeAndLongitude];
+
+    if ([self.busStop.intermodal isEqualToString:@"Metra"])
+    {
+        self.busStopImageView.image = [UIImage imageNamed:@"bus-stop-green"];
+    }
+    else if ([self.busStop.intermodal isEqualToString:@"Pace"])
+    {
+        self.busStopImageView.image = [UIImage imageNamed:@"bus-stop-purple"];
+    }
+    else
+    {
+        self.busStopImageView.image = [UIImage imageNamed:@"bus-stop-red"];
+        self.titleIntermodalLabel.hidden = YES;
+        self.intermodalLabel.hidden = YES;
+    }
+}
+
+- (void)setAddressFromLatidudeAndLongitude
+{
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:self.busStop.coordinate.latitude longitude:self.busStop.coordinate.longitude];
+    CLGeocoder *geoCoder = [CLGeocoder new];
+    [geoCoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error)
+    {
+        CLPlacemark *placemark = [placemarks firstObject];
+        NSString *address = [NSString stringWithFormat:@"%@ %@ %@ %@",
+                             placemark.thoroughfare,
+                             placemark.locality,
+                             placemark.administrativeArea,
+                             placemark.postalCode];
+        self.addressLabel.text = address;
+    }];
 }
 
 
